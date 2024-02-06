5806ab6d6782009e495f46ac8e27d3c339f0f01d
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2024-02-07 06:16:33 +0900

    chore: library

--- App/test_0-intro.py
+++ App/test_0-intro.py
@@ -10,7 +10,6 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 options = Options()
 options.add_argument('--disable-popup-blocking')
